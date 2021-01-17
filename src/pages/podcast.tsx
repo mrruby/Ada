247e95611d76565e1057ffd29006c6b60812b590
Dawid Urbas <dawid.urbas@datacave.ch>
2021-01-17 22:48:29 +0100

    Add thumbnail
    Former-commit-id: 9b4d2592fac6f2f1040bbac6f11ce67de6bec00e

--- src/pages/podcast.tsx
+++ src/pages/podcast.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
