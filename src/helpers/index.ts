0a5cbad9a401f5e0fc1fc7a105e5e6372ebd075f
Dawid Urbas <durbas92@gmail.com>
2021-05-03 22:04:43 +0200

    Fix issue
    Former-commit-id: b6fe3b3493c91cdc9502ed381dc46867234a9361

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,5 @@ export const getTitlePath = (title: string): string =>
   title
     .trim()
     .replace(/\s+/g, "-")
-    .replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>{}[\]\\/]/gi, "")
+    .replace(/[`~!@#$%^&*()_|+=?;:'",.<>{}[\]\\/]/gi, "")
     .toLowerCase()
