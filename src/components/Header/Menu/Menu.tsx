8dcc65ce41ceb695b2efe20da1fa4603c6ebc23a
Dawid Urbas <durbas92@gmail.com>
2022-10-27 10:06:38 +0200

    Change url add quiz
    Former-commit-id: 4f8c4d0035f168a6ba44dad389e01271535dcd45

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -12,10 +12,7 @@ const Menu = ({ open, setOpen }: Props): JSX.Element => {
   return (
     <>
       <StyledMenu open={open} aria-hidden={!open}>
-        <a
-          className="highlight"
-          href="https://chce-robic-lepsze-kampanie.adrianna.com.pl/"
-        >
+        <a className="highlight" href="https://ebook.adrianna.com.pl/">
           ebook
         </a>
         <a href="https://checklistafb.adrianna.com.pl">checklista</a>
@@ -23,6 +20,7 @@ const Menu = ({ open, setOpen }: Props): JSX.Element => {
         <Link to="/about">O mnie</Link>
         <Link to="/podcast">Podcast</Link>
         <Link to="/blog">Blog</Link>
+        {/* <Link to="/quiz">Quiz</Link> */}
         <Link to="/contact">Kontakt</Link>
       </StyledMenu>
       <Burger open={open} setOpen={setOpen} />
