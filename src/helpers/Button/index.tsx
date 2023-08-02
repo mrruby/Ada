d7d514e7e6fd055e24f17d5088ed533e0c27d4b0
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-08-02 13:41:47 +0200

    update styles, add helpers
    Former-commit-id: 668e2e5eb6880f4f590b74eabf759cec9301b31c

---
+++ src/helpers/Button/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface Props {
+  type: "button" | "submit" | "reset";
+  text: string;
+  textSize?: string;
+  sectionId?: string;
+}
+
+export const Button: React.FC<Props> = ({ type, text, 
+  textSize = "lg:text-[25px]",
+  sectionId
+}) => {
+  const handleButtonClick = () => {
+    if (sectionId) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+
+  return (
+    <button
+      onClick={handleButtonClick}
+      type={type}
+      className={`bg-ada-light-pink ${textSize} px-[38px] py-[6px] mt-[80px]`}    
+      >
+      {text}
+    </button>
+  );
+};
