780f5391c7d44353d69e155df571fe989b1ad8d0
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-09-04 21:26:55 +0200

    update styling hompage and about
    Former-commit-id: 54a717eab9209d7e92532898a735abc443c278d0

--- src/components/CustomBanner/index.tsx
+++ src/components/CustomBanner/index.tsx
@@ -7,9 +7,12 @@ type Props = {
   paragraph1: JSX.Element
   paragraph2?: JSX.Element
   paragraph3?: string
+  paragraphStyle?: string
   actionButton?: boolean
   btnText: string
   image: JSX.Element
+  imgStyle?: string
+  btnUrl: string
 }
 
 const CustomBanner = ({
@@ -18,23 +21,26 @@ const CustomBanner = ({
   paragraph1,
   paragraph2,
   paragraph3,
+  paragraphStyle,
   btnText,
   image,
+  imgStyle = "lg:absolute bottom-0 right-[-50px]",
+  btnUrl 
 }: Props): JSX.Element => {
   const subtitleClass = "text-adaSubtitle font-medium leading-tight py-2";
 
   return (
-    <div className="flex px-4 lg:pl-[110px] flex-col items-center lg:flex-row text-ada-blue relative min-h-[660px]">
-        <div className="lg:w-[678px]">
-          <h1 className="text-adaTitle font-bold pb-8 lg:w-[704px]">{title}</h1>
+    <div className="flex px-4 flex-col items-center lg:flex-row text-ada-blue relative min-h-[740px] max-w-[1280px] mx-auto">
+        <div className={paragraphStyle}>
+          <h1 className="text-adaTitle font-bold pb-8 lg:w-[704px] {titleStyle}">{title}</h1>
           <p className={subtitleClass}>{paragraph1}</p>
           <p className={subtitleClass}>{paragraph2}</p>
           <p className={subtitleClass}>{paragraph3}</p>
           <div className="mt-[14px]">
-          {actionButton && <Button type="button" text={btnText} url="https://sklep.adrianna.com.pl/search"/>}
+          {actionButton && <Button type="button" text={btnText} url={btnUrl}/>}
           </div>
         </div>
-        <div className="lg:absolute top-0 right-[-50px]">
+        <div className={imgStyle}>
           {image}
         </div>
     </div>
--- src/helpers/LayoutElements/index.tsx
+++ src/helpers/LayoutElements/index.tsx
@@ -129,3 +129,49 @@ export const FlowerBlue2: React.FC<Props> = ({
     />
   );
 };
+
+export const FlowerBlue3: React.FC<Props> = ({
+  top,
+  right,
+  left,
+  bottom,
+  mobileTop,
+  mobileRight,
+  mobileLeft,
+  mobileBottom,
+}) => {
+  return (
+    <img
+    src="../assets/layout-elements/flower-blue.svg"
+    alt="Blue Flower - Ada Promis"
+      className={`absolute ${top && top} ${right && right} ${left && left} ${bottom && bottom
+        } ${mobileTop && mobileTop} ${mobileRight && mobileRight} ${mobileLeft && mobileLeft
+        } ${mobileBottom && mobileBottom} z-20`}
+      width={150}
+      height={150}
+    />
+  );
+};
+
+export const TwoBlueFlowers2: React.FC<Props> = ({
+  top,
+  right,
+  left,
+  bottom,
+  mobileTop,
+  mobileRight,
+  mobileLeft,
+  mobileBottom,
+}) => {
+  return (
+    <img
+      src="../assets/layout-elements/two-flowers.svg"
+      alt="Two Flowers - Ada Promis"
+      className={`absolute ${top && top} ${right && right} ${left && left} ${bottom && bottom
+        } ${mobileTop && mobileTop} ${mobileRight && mobileRight} ${mobileLeft && mobileLeft
+        } ${mobileBottom && mobileBottom} z-20 opacity-0 lg:opacity-100`}
+      width={230}
+      height={300}
+    />
+  );
+};
