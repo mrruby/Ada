28ac413b9151e53738770dc7bdf2e74c9b598cf5
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-09-06 17:01:50 +0200

    add images to BlogPost
    Former-commit-id: 824dade99997f6ec895a6d6f7ca3aa7427809e6f

--- src/helpers/BlogPost/index.tsx
+++ src/helpers/BlogPost/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Button } from "helpers/Button";
+import { StaticImage } from "gatsby-plugin-image";
 
 type BlogPostProps = {
   tags: string[];
   title: string;
   date: string;
+  thumbnail: string;
   slug: string;
 }
 
-export const BlogPost: React.FC<BlogPostProps> = ({ tags, title, date, slug, 
+export const BlogPost: React.FC<BlogPostProps> = ({ tags, title, date,thumbnail, slug, 
 }) => {
 
   function formatDate(date:string) {
@@ -22,9 +24,16 @@ export const BlogPost: React.FC<BlogPostProps> = ({ tags, title, date, slug,
   return (
     <div className="bg-white mx-6 lg:w-[320px] min-h-[400px] flex flex-col items-center justify-between relative mt-[24px] pt-[58px] lg:pb-[40px] mb-[60px]" >
       <div className="text-center lg:w-[274px]">
-        <p className="font-bold text-adaNav text-ada-light-pink">{tags.map((tag) => `#${tag} `).join("")}</p>  
-        <h3 className="font-semibold text-adaDesc py-[24px]">{title}</h3>
+        <p className="font-bold text-adaNav text-ada-dark-pink">{tags.map((tag) => `#${tag} `).join("")}</p>  
+        <h3 className="font-semibold text-adaDesc py-[24px] min-h-[130px]">{title}</h3>
         <p className="font-bold text-[10px] pb-[24px]">{formattedDate}</p>  
+        <div className="flex max-h-[240px] max-w-[240px] md:h-[320px] md:max-w-[320px] mx-auto mb-[16px] md:mb-[32px]" >
+        <img
+          src={thumbnail}
+          alt={title}
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+        </div>
       </div>
       <Button type="button" text="Czytaj dalej..." url={slug} border={true} textSize="lg:text-adaBase" />
     </div>
