54efb67fa64f7a2852ba3c3c43380ec41439b00e
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-09-06 13:33:14 +0200

    styling  blog posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Header = (): JSX.Element => {
   const [open, setOpen] = useState(false)
 
   return (
-    <MaxWithBgColorContainer bgColor="bg-ada-light-pink">
+    <MaxWithBgColorContainer bgColor="bg-ada-light-pink" extraStyle="fixed z-50">
     <header>
         <Menu open={open} setOpen={setOpen} />
         <MenuDesktop />
