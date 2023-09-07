431a079b712e6f5d3689bd8f011b1a547f6e3fe0
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2023-09-07 12:45:08 +0200

    fix bug with SSR
    Former-commit-id: 810f63a1094e5f5f8bc347a8f672e6cbb38fa550

--- src/components/BlogPostLayout/index.tsx
+++ src/components/BlogPostLayout/index.tsx
@@ -1,71 +1,65 @@
 import React from "react";
-
+import ReactHtmlParser, { Transform } from "react-html-parser";
 type Props = {
   title: string;
   html: string;
   date: string;
   tags: string[];
-}
-
+};
 const BlogPostLayout = ({ title, html, date, tags }: Props): JSX.Element => {
-  const addStylesToElements = (htmlString: string) => {
-    const div = document.createElement("div");
-    div.innerHTML = htmlString;
-
-    const h2Elements = div.querySelectorAll("h2");
-    const h3Elements = div.querySelectorAll("h3");
-    const h4Elements = div.querySelectorAll("h4");
-    const pElements = div.querySelectorAll("p");
-    const listElements = div.querySelectorAll("ul, ol");
-    const imgElements = div.querySelectorAll("img");
-
-    h2Elements.forEach((h2) => {
-      h2.classList.add("font-bold", "md:text-adaSubtitle", "py-3"); 
-    });
-
-    h3Elements.forEach((h3) => {
-      h3.classList.add("font-bold", "py-3"); 
-    });
-
-    h4Elements.forEach((h4) => {
-      h4.classList.add("font-bold", "py-3"); 
-    });
-
-    pElements.forEach((p) => {
-      p.classList.add("py-2"); 
-    });
-
-    listElements.forEach((list) => {
-      list.classList.add("list-disc", "pl-6", "pb-4"); 
-    });
-
-    imgElements.forEach((img) => {
-      img.classList.add("pb-[20px]", "w-full", "h-full",  ); 
-    });
-
-    return div.innerHTML;
+  const transform: Transform = (node, index) => {
+    if (node.type === 'tag') {
+      switch (node.name) {
+        case 'h2':
+          return <h2 key={index} className="font-bold md:text-adaSubtitle py-3">{node.children[0].data}</h2>;
+      case 'h3':
+          return <h3 key={index} className="font-bold py-3 ">{node.children[0].data}</h3>;
+      case 'h4':
+          return <h4 key={index} className="font-bold py-3 ">{node.children[0].data}</h4>; 
+      // case 'p':
+      //       return <p key={index} className="py-2">{node.children[0].data}</p>;     
+      // case 'img':
+      //       return <img key={index} className="pb-[20px] w-fullh-full" src={node.children[0].data}></img>;      
+      // case 'ol':
+      //       return (
+      //         <ol key={index} className="list-decimal pl-6 pb-4">
+      //           {node.children.map((child, childIndex) => (
+      //             <li key={childIndex}>{ReactHtmlParser(child.data)}</li>
+      //           ))}
+      //         </ol>
+      //       );
+      // case 'ul':
+      //       return (
+      //         <ol key={index} className="list-disc pl-6 pb-4">
+      //           {node.children.map((child, childIndex) => (
+      //             <ul key={childIndex}>{ReactHtmlParser(child.data)}</ul>
+      //           ))}
+      //         </ol>
+      //       );      
+      default:
+          return;
+      }
+    }
   };
 
-  const sanitizedHtml = addStylesToElements(html);
-
-  function formatDate(date:string) {
-    const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: '2-digit', year: 'numeric' };
-    const dateObject = new Date(date);
-    const formattedDate = dateObject.toLocaleDateString('en-UK', options).toUpperCase();
-    return formattedDate.replace(/\//g, '.');
-  }
-  const formattedDate = formatDate(date)
-
+  const transformedHtml = ReactHtmlParser(html, { transform });
+  const formattedDate = new Date(date)
+    .toLocaleDateString("en-UK", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    })
+    .toUpperCase()
+    .replace(/\//g, ".");
   return (
     <div className="max-w-7xl mx-auto px-2 lg:px-5 pb-10">
       <h1 className="font-bold text-adaSubtitle md:text-adaSubtitleSecondary pb-4 pt-6">{title}</h1>
       <div className="flex flex-col md:flex-row justify-between">
-        <p className="font-bold text-adaNav text-ada-dark-pink pb-4">{tags.map((tag) => `#${tag} `).join("")}</p>       
-        <p className="font-bold text-adaNav pb-[24px]">{formattedDate}</p>  
+        <p className="font-bold text-adaNav text-ada-dark-pink pb-4">{tags.map((tag) => `#${tag} `).join("")}</p>
+        <p className="font-bold text-adaNav pb-[24px]">{formattedDate}</p>
       </div>
-      <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
+      <div>{transformedHtml}</div>
     </div>
   );
 };
-
 export default BlogPostLayout;
