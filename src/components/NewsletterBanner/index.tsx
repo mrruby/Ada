5ed729fcfb8ae587ffadb331029ed98383f57f4f
Dawid Urbas <durbas92@gmail.com>
2023-09-29 14:35:03 +0200

    Fix image issues

--- src/components/NewsletterBanner/index.tsx
+++ src/components/NewsletterBanner/index.tsx
@@ -3,15 +3,15 @@ import { Button } from "helpers/Button"
 import { StaticImage } from "gatsby-plugin-image"
 
 type Props = {
-  title1?: boolean;
-  title2?: boolean;
-  title3?: boolean;
-  title4?: boolean;
-  paragraph?: boolean;
-  paragraph2?: boolean;
-  twoParagraphs?: boolean; 
+  title1?: boolean
+  title2?: boolean
+  title3?: boolean
+  title4?: boolean
+  paragraph?: boolean
+  paragraph2?: boolean
+  twoParagraphs?: boolean
   btnText?: string
-  image?: boolean;
+  image?: boolean
 }
 
 const NewsletterBanner = ({
@@ -23,40 +23,52 @@ const NewsletterBanner = ({
   paragraph2,
   twoParagraphs,
   btnText = "Zapisuję się, zanim wyjdzie kolejny mail!",
-  image
+  image,
 }: Props): JSX.Element => {
-  const h2ClassName = "text-adaDesc lg:text-adaTitle font-bold lg:mb-2 border-b-[16px] lg:border-b-[34px] border-ada-light-pink h-[24px] lg:h-[68px]";
+  const h2ClassName =
+    "text-adaDesc lg:text-adaTitle font-bold lg:mb-2 border-b-[16px] lg:border-b-[34px] border-ada-light-pink h-[24px] lg:h-[68px]"
 
   return (
-    <div className="flex items-left flex-col pt-10 md:pb-14 relative min-h-[500px] max-w-7xl mx-auto px-2">
-      { title1 && (
+    <div className="flex items-left flex-col pt-10 relative min-h-[500px] max-w-7xl mx-auto px-2">
+      {title1 && (
         <>
-          <h2 className={`lg:max-w-[780px] ${h2ClassName}`}>Chcesz wiedzieć więcej o slow</h2>
-          <h2 className={`lg:max-w-[650px] ${h2ClassName}`}>marketingu i reklamach?</h2>
+          <h2 className={`lg:max-w-[780px] ${h2ClassName}`}>
+            Chcesz wiedzieć więcej o slow
+          </h2>
+          <h2 className={`lg:max-w-[650px] ${h2ClassName}`}>
+            marketingu i reklamach?
+          </h2>
         </>
-      )
-      }
-      { title2 && (
+      )}
+      {title2 && (
         <>
-          <h2 className={`lg:max-w-[1050px] ${h2ClassName}`}>Praktykuję slow marketing: przemyślany,</h2>
-          <h2 className={`lg:max-w-[980px] ${h2ClassName}`}> wartościowy, zbudowany na relacjach</h2>
+          <h2 className={`lg:max-w-[1050px] ${h2ClassName}`}>
+            Praktykuję slow marketing: przemyślany,
+          </h2>
+          <h2 className={`lg:max-w-[980px] ${h2ClassName}`}>
+            {" "}
+            wartościowy, zbudowany na relacjach
+          </h2>
         </>
-      )
-      }
-      { title3 && (
+      )}
+      {title3 && (
         <>
-          <h2 className={`lg:max-w-[590px] ${h2ClassName}`}>Zostańmy w kontakcie</h2>
-          <h2 className={`lg:max-w-[740px] ${h2ClassName}`}>nie przegap żadnego wpisu!</h2>
+          <h2 className={`lg:max-w-[590px] ${h2ClassName}`}>
+            Zostańmy w kontakcie
+          </h2>
+          <h2 className={`lg:max-w-[740px] ${h2ClassName}`}>
+            nie przegap żadnego wpisu!
+          </h2>
         </>
-      )
-      }
-      { title4 && (
+      )}
+      {title4 && (
         <>
-          <h2 className={`lg:max-w-[566px] ${h2ClassName}`}>Zaglądaj tutaj często</h2>
+          <h2 className={`lg:max-w-[566px] ${h2ClassName}`}>
+            Zaglądaj tutaj często
+          </h2>
           <h2 className={`lg:max-w-[335px] ${h2ClassName}`}>po nowości!</h2>
         </>
-      )
-      }
+      )}
       {paragraph && (
         <p className="lg:text-adaBase mt-[30px]">
           Zapisz się do newslettera i odbieraj maile z wiedzą.
@@ -64,31 +76,46 @@ const NewsletterBanner = ({
       )}
       {paragraph2 && (
         <p className="lg:text-adaSubtitle mt-[30px] lg:max-w-[641px]">
-          Chcesz być na bieżąco? Zapisz się na slowmarketingowy newsletter i zdobądź wiedzę o robieniu marketingu w zgodzie ze sobą!        
-      </p>
+          Chcesz być na bieżąco? Zapisz się na slowmarketingowy newsletter i
+          zdobądź wiedzę o robieniu marketingu w zgodzie ze sobą!
+        </p>
       )}
-    {twoParagraphs && (
-    <div>
-      <p className="lg:text-adaBase mt-[30px]">
-      W kampaniach reklamowych buduję świadomość, wzbudzam zainteresowanie i sprawiam, aby klient Cię polubił.          
-      </p>
-      <p className="lg:text-adaBase mt-[30px]">
-      Zgaduję, że Ty też chętniej kupujesz od marek, do których czujesz 
-      <span className="font-bold"> sympatię</span>
-      ? :)
-      </p>
-    </div>
-    )}
+      {twoParagraphs && (
+        <div>
+          <p className="lg:text-adaBase mt-[30px]">
+            W kampaniach reklamowych buduję świadomość, wzbudzam zainteresowanie
+            i sprawiam, aby klient Cię polubił.
+          </p>
+          <p className="lg:text-adaBase mt-[30px]">
+            Zgaduję, że Ty też chętniej kupujesz od marek, do których czujesz
+            <span className="font-bold"> sympatię</span>? :)
+          </p>
+        </div>
+      )}
       <div className="mt-[28px]">
-        <Button type="button" text={btnText} border={true} url="https://metanewsletter.adrianna.com.pl" textSize="lg:text-adaBase"/>
+        <Button
+          type="button"
+          text={btnText}
+          border={true}
+          url="https://metanewsletter.adrianna.com.pl"
+          textSize="lg:text-adaBase"
+        />
       </div>
       {image && (
-        <div className="md:absolute top-[290px] lg:top-[-10px] right-[-150px]">
-        <StaticImage src={"../../images/ada_homepage.png"} alt="Ada Promis" placeholder="none" width={700} height={700}  />
+        <div className="md:absolute top-0 right-0 xl:right-[50px]">
+          <StaticImage
+            src={"../../images/ada_homepage.webp"}
+            alt="Ada Promis"
+            placeholder="blurred"
+            formats={["auto", "webp", "avif"]}
+            width={500}
+            height={500}
+            quality={95}
+          />
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default NewsletterBanner;
+export default NewsletterBanner
--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,24 +1,42 @@
-import React from "react";
-import { ProductBox } from "helpers/ProductBox";
-import { StaticImage } from "gatsby-plugin-image";
+import React from "react"
+import { ProductBox } from "helpers/ProductBox"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Products = (): JSX.Element => {
   const productsData = [
     {
       title: "Konsultacje reklamowe",
-      description:"Prowadź kampanie reklamowe pod moim okiem i rozwijaj biznes.",
-      buttonText: 'Pokaż ofertę konsultacji dla mnie',
-      url: 'https://sklep.adrianna.com.pl/search?q=konsultacja',
-      image: <StaticImage src={"../../images/ada-konsultacje.png"} alt="Ada Promis" placeholder="none" width={300} height={300} />
+      description:
+        "Prowadź kampanie reklamowe pod moim okiem i rozwijaj biznes.",
+      buttonText: "Pokaż ofertę konsultacji dla mnie",
+      url: "https://sklep.adrianna.com.pl/search?q=konsultacja",
+      image: (
+        <StaticImage
+          src={"../../images/ada-konsultacje.png"}
+          alt="Ada Promis"
+          placeholder="none"
+          width={300}
+          height={300}
+        />
+      ),
     },
     {
       title: "Prowadzenie kampanii",
-      description: "Oddeleguj pozyskiwanie klientów i ciesz się czasem dla Ciebie.",
-      buttonText: 'Poprowadzę Twoje kampanie reklamowe',
-      url: '/contact',
-      image: <StaticImage src={"../../images/ada-kampanie.png"} alt="Ada Promis" placeholder="none" width={300} height={300} />    
+      description:
+        "Oddeleguj pozyskiwanie klientów i ciesz się czasem dla Ciebie.",
+      buttonText: "Poprowadzę Twoje kampanie reklamowe",
+      url: "/contact",
+      image: (
+        <StaticImage
+          src={"../../images/ada-kampanie.webp"}
+          alt="Ada Promis"
+          placeholder="none"
+          width={300}
+          height={300}
+        />
+      ),
     },
-  ];
+  ]
 
   return (
     <div className="flex flex-col md:flex-row items-center w-full px-4 lg:px-10 lg:gap-[32px]">
@@ -29,11 +47,11 @@ const Products = (): JSX.Element => {
           description={item.description}
           buttonText={item.buttonText}
           url={item.url}
-          image={item.image} 
+          image={item.image}
         />
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
