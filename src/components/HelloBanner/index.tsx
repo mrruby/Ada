fb00b1435f8f742864e7200d29a440b35dc2d955
Dawid Urbas <dawid@getbold.agency>
2023-11-23 17:41:43 +0100

    Optimize images

--- src/components/HelloBanner/index.tsx
+++ src/components/HelloBanner/index.tsx
@@ -1,16 +1,29 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const HelloBanner = (): JSX.Element => {
   return (
-    <div className="w-full bg-adapromis h-[500px] md:h-[690px] xl:h-[850px] flex flex-col items-center justify-center">
-      <h2 className="text-[40px] lg:text-[96px] font-bold text-center animate-bounce">
-        Cześć, tu Ada Promis!
-      </h2>
-      <p className="max-w-[726px] text-center font-medium text-[14px] md:text-adaBase">
-        Pracuję z biznesami, które dbają o dobrostan psychiczny i fizyczny:
-        praktykuję slow marketing, przemyślany, wartościowy, zbudowany na
-        relacjach
-      </p>
+    <div className="grid h-[500px] md:h-[690px] xl:h-[850px]">
+      <StaticImage
+        className="row-start-1 col-start-1"
+        layout="fullWidth"
+        alt=""
+        src={"../../images/bg-adapromis.png"}
+        formats={["auto", "webp", "avif"]}
+        quality={100}
+      />
+      <div className="row-start-1 col-start-1 grid justify-center items-center pt-40">
+        <div className="flex flex-col items-center z-20">
+          <h2 className="text-[40px] lg:text-[96px] font-bold text-center animate-bounce">
+            Cześć, tu Ada Promis!
+          </h2>
+          <p className="max-w-[726px] text-center font-medium text-[14px] md:text-adaBase mx-auto">
+            Pracuję z biznesami, które dbają o dobrostan psychiczny i fizyczny:
+            praktykuję slow marketing, przemyślany, wartościowy, zbudowany na
+            relacjach
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/MastermindBanner/index.tsx
+++ src/components/MastermindBanner/index.tsx
@@ -1,37 +1,48 @@
 import React from "react"
 import { Button } from "helpers/Button"
+import { StaticImage } from "gatsby-plugin-image"
 
 const MastermindBanner = (): JSX.Element => {
   return (
-    <div className="w-full bg-mastermind h-[500px] md:h-[690px] xl:h-[760px] flex flex-col items-center justify-center px-2">
-      <div className="max-w-[1072px] flex flex-col items-center">
-        <h1 className="text-adaBase lg:text-adaTitle font-bold text-center leading-tight animate-bounce pb-8">
-          Chcesz, aby więcej osób kupowało Twoje produkty i korzystało z Twoich
-          usług?
-        </h1>
-        <p className="max-w-[980px] mx-auto text-center font-medium text-[14px] md:text-adaSubtitle leading-tight">
-          Dołącz do programu mentoringowego „Ogarnij swoje adsy!” i osiągaj w
-          kampaniach reklamowych wyniki, na widok których aż przetrzesz oczy!
-          Dobrze zaplanowane kampanie reklamowe pracują „same”. Przyciągną do
-          Ciebie nowych klientów nawet wtedy, gdy śpisz, jesteś na urlopie czy
-          gdy spędzasz czas z rodziną.
-        </p>
-        <p className="p-[30px] max-w-[980px] mx-auto text-center font-medium text-[14px] md:text-adaSubtitle leading-tight">
-          Świetne wyniki reklamowe = lepsze pieniądze = lepsze życie, prawda? ;)
-        </p>
-        <Button
-          type="button"
-          text={
-            <>
-              Chcę
-              <span className="font-bold"> ulepszyć </span>
-              moje kampanie!
-            </>
-          }
-          sectionId="mastermind-info"
-          textSize="md:text-adaSubtitle"
-          btnStyle="md:w-[425px] scroll-smooth"
-        />
+    <div className="grid relative h-[500px] md:h-[690px]">
+      <StaticImage
+        className="row-start-1 col-start-1"
+        layout="fullWidth"
+        alt=""
+        src={"../../images/bg-mastermind.png"}
+        formats={["auto", "webp", "avif"]}
+      />
+      <div className="row-start-1 col-start-1 grid relative justify-center pt-40">
+        <div className="max-w-[1072px] flex flex-col items-center z-20">
+          <h1 className="text-adaBase lg:text-adaTitle font-bold text-center leading-tight animate-bounce pb-8">
+            Chcesz, aby więcej osób kupowało Twoje produkty i korzystało z
+            Twoich usług?
+          </h1>
+          <p className="max-w-[980px] mx-auto text-center font-medium text-[14px] md:text-adaSubtitle leading-tight">
+            Dołącz do programu mentoringowego „Ogarnij swoje adsy!” i osiągaj w
+            kampaniach reklamowych wyniki, na widok których aż przetrzesz oczy!
+            Dobrze zaplanowane kampanie reklamowe pracują „same”. Przyciągną do
+            Ciebie nowych klientów nawet wtedy, gdy śpisz, jesteś na urlopie czy
+            gdy spędzasz czas z rodziną.
+          </p>
+          <p className="p-[30px] max-w-[980px] mx-auto text-center font-medium text-[14px] md:text-adaSubtitle leading-tight">
+            Świetne wyniki reklamowe = lepsze pieniądze = lepsze życie, prawda?
+            ;)
+          </p>
+          <Button
+            type="button"
+            text={
+              <>
+                Chcę
+                <span className="font-bold"> ulepszyć </span>
+                moje kampanie!
+              </>
+            }
+            sectionId="mastermind-info"
+            textSize="md:text-adaSubtitle"
+            btnStyle="md:w-[425px] scroll-smooth"
+          />
+        </div>
       </div>
     </div>
   )
