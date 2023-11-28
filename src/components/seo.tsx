d6633d319760ab5cef336dcd1cfd686659e3b75a
Dawid Urbas <dawid@getbold.agency>
2023-11-28 13:13:18 +0100

    Fix seo

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -22,23 +22,22 @@ const SEO = ({ description, title }: Props): JSX.Element => {
   )
 
   const metaDescription = description || site.siteMetadata.description
+  const image = "https://adrianna.com.pl/img/og_mastermind.webp"
 
   return (
     <>
       <html lang="pl-PL" />
       <title>{`${title} | ${site.siteMetadata.title}`}</title>
       <meta name="description" content={metaDescription} />
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:image"
-        content={"https://adrianna.com.pl/img/og_mastermind.webp"}
-      />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata.author} />
+      <meta property="og:image" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="adrianna.com.pl" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content={image} />
       <meta
         name="facebook-domain-verification"
         content="lghw23ob63nz7oal7ll1jo6ys8txt3"
