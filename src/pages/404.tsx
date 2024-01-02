14a73aade8e3e1447b812bad55db04182846ccf4
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2024-01-02 12:18:24 +0100

    update company data and text on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,18 +2,24 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import MaxWithBgColorContainer from "components/Layout/MaxWithBgColorContainer"
+import { Button } from "helpers/Button"
 
 const NotFoundPage = (): JSX.Element => (
   <Layout>
-    <MaxWithBgColorContainer extraStyle="h-[450px] md:h-[820px] pt-[60px] flex flex-col justify-center text-center">
-      <h1 className="px-4 text-adaTitle font-bold pb-8">NOT FOUND</h1>
-      <p className="lg:text-adaBase pt-[24px] font-medium leading-tight">
-        You just hit a route that doesn&#39;t exist... the sadness.
-      </p>
+    <MaxWithBgColorContainer extraStyle="h-[450px] md:h-[900px] pt-[60px] flex flex-col justify-center text-center">
+      <h1 className="px-4 text-adaTitle font-bold pb-8">
+        Ta strona nie istnieje, błąd 404.
+      </h1>
+      <Button
+        type="button"
+        text="Przejdź na stronę główną"
+        url="/"
+        btnStyle="md:w-[440px]"
+      />
     </MaxWithBgColorContainer>
   </Layout>
 )
 
-export const Head = () => <SEO title="404: Not found" />
+export const Head = () => <SEO title="404: Ta strona nie istnieje" />
 
 export default NotFoundPage
