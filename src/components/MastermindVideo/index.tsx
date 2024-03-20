7f0506d7f3d5564e397851d2f18afaaa24d2e924
Dawid Urbas <dawid@getbold.agency>
2024-03-20 13:27:21 +0100

    Fix powtorka page

--- src/components/MastermindVideo/index.tsx
+++ src/components/MastermindVideo/index.tsx
@@ -17,7 +17,7 @@ const MastermindVideo = ({
     <div className="flex flex-col pb-20 pt-20 md:pt-14 gap-10 px-2">
       <div className="flex flex-col items-center gap-2 mx-auto pb-8">
         <h2 className="text-adaBase lg:text-[36px] font-bold text-center leading-tight animate-bounce pt-5">
-          Do końca zapisów na program zostało:
+          ZAREZERWUJ SPOTKANIE DO ☕
         </h2>
         <CountdownTimer targetDate={targetDate} />
         <div className="flex flex-col items-center pt-20">
--- src/pages/powtorka-webinaru.tsx
+++ src/pages/powtorka-webinaru.tsx
@@ -1,8 +1,21 @@
 import React from "react"
-import Layout from "../components/Layout"
-import SEO from "../components/seo"
+import Layout from "components/Layout"
+import SEO from "components/seo"
 import MaxWithBgColorContainer from "components/Layout/MaxWithBgColorContainer"
 import MastermindVideo from "components/MastermindVideo"
+import IconText from "components/IconText"
+import MentoringBanner from "components/MentoringBanner"
+import MentoringWhy from "components/MentoringWhy"
+import Opinions from "components/Opinions"
+import MentoringWhy2 from "components/MentoringWhy2"
+import MastermindAgenda from "components/MastermindAgenda"
+import MastermindSteps from "components/MastermindSteps"
+import MentoringInfo from "components/MentoringInfo"
+import MentoringAbout from "components/MentoringAbout"
+import ReferencesMentoring from "components/ReferencesMentoring"
+import MastermindInfo3 from "components/MastermindInfo3"
+import MastermindPrice from "components/MastermindPrice"
+import MasterclassFAQ from "components/MasterclassFAQ"
 
 const WebinarReplayPage = (): JSX.Element => {
   return (
@@ -11,8 +24,50 @@ const WebinarReplayPage = (): JSX.Element => {
         <MastermindVideo
           videoUrl="https://www.youtube.com/embed/y_HOSt8dcy4?si=aKR8v1BbLwZZ1cZq"
           buttonUrl="https://koalendar.com/e/meta-ads-master/"
-          targetDate={new Date("2024-03-27T00:00:00")}
+          targetDate={new Date("2024-04-03T00:00:00")}
         />
+      </MaxWithBgColorContainer>
+      <IconText />
+      <MaxWithBgColorContainer bgColor="bg-ada-light-pink">
+        <MentoringBanner version={1} />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-linear2">
+        <MentoringWhy />
+      </MaxWithBgColorContainer>
+      <Opinions />
+      <div className="h-[300px] bg-wave absolute top-[4180px] w-screen"></div>
+      <MaxWithBgColorContainer bgColor="bg-linear3">
+        <MentoringWhy2 />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer>
+        <MastermindAgenda version={2} />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-linear3">
+        <MastermindSteps version={2} />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-linear2">
+        <MentoringInfo />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-wave">
+        <MentoringBanner version={2} />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-linear3">
+        <MentoringAbout />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer>
+        <ReferencesMentoring />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-wave">
+        <MentoringBanner version={3} />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer>
+        <MastermindInfo3 version={2} />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-linear3">
+        <MastermindPrice version={2} />
+      </MaxWithBgColorContainer>
+      <MaxWithBgColorContainer bgColor="bg-linear2">
+        <MasterclassFAQ version={2} />
       </MaxWithBgColorContainer>
     </Layout>
   )
