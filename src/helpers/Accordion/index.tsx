c3ebd5a4091137c26e5a71c8443f1441bbe935a0
Dawid Urbas <dawid@getbold.agency>
2024-07-02 18:00:26 +0200

    Fix buttons

--- src/helpers/Accordion/index.tsx
+++ src/helpers/Accordion/index.tsx
@@ -10,11 +10,11 @@ export const Accordion: React.FC<AccordionProps> = ({ question, answer }) => {
   const [accordionOpen, setAccordionOpen] = useState(false)
 
   return (
-    <div
-      className="xl:w-[550px]"
-      onClick={() => setAccordionOpen(!accordionOpen)}
-    >
-      <button className="flex lg:text-adaBase font-bold py-4 px-4  items-center justify-between w-full text-center mx-auto shadow-lg">
+    <div className="xl:w-[550px]">
+      <button
+        className="flex cursor-pointer lg:text-adaBase font-bold p-4 items-center justify-between w-full text-center mx-auto shadow-lg"
+        onClick={() => setAccordionOpen(!accordionOpen)}
+      >
         <h2 className="text-left">{question}</h2>
         <svg
           className="fill-ada-blue shrink-0 ml-8"
