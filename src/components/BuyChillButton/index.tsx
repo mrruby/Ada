61d826c7fea9563c56a0785c1e381e150780fc8e
Dawid Urbas <dawid@getbold.agency>
2024-08-08 18:11:47 +0200

    Use client

---
+++ src/components/BuyChillButton/index.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from "react"
+import { createCartAndRedirectToCheckout } from "utils/shopify"
+
+const BuyChillButton = (): JSX.Element => {
+  return (
+    <button
+      onClick={async () => {
+        try {
+          const checkoutUrl = await createCartAndRedirectToCheckout(
+            "49088728006997",
+            1
+          )
+          window.location.href = checkoutUrl
+        } catch (error) {
+          console.error(
+            "Error creating cart and redirecting to checkout:",
+            error
+          )
+        }
+      }}
+      type="button"
+      className="bg-ada-pink2 mt-2 mb-20 md:my-10 mx-auto border-b-4 border-ada-pink font-medium px-5 py-1.5 hover:bg-ada-pink hover:shadow-xl"
+    >
+      <span className="font-extrabold uppercase">korzystam</span>
+    </button>
+  )
+}
+
+export default BuyChillButton
