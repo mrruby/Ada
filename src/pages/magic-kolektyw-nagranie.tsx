14e9fddff1690daa93bb0ad21624b798ac9a2e97
Dawid Urbas <durbas92@gmail.com>
2025-04-18 12:24:05 +0200

    Fix

--- src/pages/magic-kolektyw-nagranie.tsx
+++ src/pages/magic-kolektyw-nagranie.tsx
@@ -1,60 +1,13 @@
-import Layout from "components/Layout"
-import MaxWithBgColorContainer from "components/Layout/MaxWithBgColorContainer"
-import MagicBanner3 from "components/MagicBanner3"
-import MagicBioBanner2 from "components/MagicBioBanner2"
-import MagicCollectiveBanner from "components/MagicCollectiveBanner"
-import MagicTitle from "components/MagicTitle"
-import MagicWebinarHero from "components/MagicWebinarHero/MagicWebinarHero"
-import ReferencesMentoring from "components/ReferencesMentoring"
 import SEO from "components/seo"
+import { navigate } from "gatsby"
 import React from "react"
 
 const MagicCollectivePage = (): JSX.Element => {
-  return (
-    <Layout showHeaderAndFooter={false}>
-      <MaxWithBgColorContainer bgColor="bg-linear2">
-        <MagicWebinarHero
-          title="Magic: Marketing Ads Girls Inside Collective"
-          countdownText="Webinar znika za:"
-          countdownTargetDate={new Date("2025-04-17T23:59:00")}
-          videoId="YUEaiE7aTaE"
-          videoTitle="Magic: Marketing Ads Girls Inside Collective"
-        />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-white2 reltive">
-        <div className="z-10 relative">
-          <MagicTitle version={3} />
-        </div>
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-purple3 z-10 relative">
-        <MagicCollectiveBanner version={1} />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-purple3">
-        <MagicCollectiveBanner version={3} />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-magicGreen">
-        <MagicBioBanner2 />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-magic-grid">
-        <MagicBanner3 />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-magic-pink">
-        <MagicCollectiveBanner version={5} />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-magicGreen">
-        <MagicCollectiveBanner version={6} />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-magicPink">
-        <MagicCollectiveBanner version={4} />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-white2">
-        <ReferencesMentoring title6 />
-      </MaxWithBgColorContainer>
-      <MaxWithBgColorContainer bgColor="bg-ada-magicPink2 bg-magicWave">
-        <MagicCollectiveBanner version={7} />
-      </MaxWithBgColorContainer>
-    </Layout>
-  )
+  React.useEffect(() => {
+    navigate("/magic-kolektyw/", { replace: true })
+  }, [])
+
+  return <></>
 }
 
 export const Head = () => (
