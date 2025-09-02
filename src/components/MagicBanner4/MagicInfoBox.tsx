3ca22128543d2b8ceae2cc6879d28a023029dc64
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2025-09-02 21:15:28 +0200

    add-magic-invitation page

---
+++ src/components/MagicBanner4/MagicInfoBox.tsx
@@ -0,0 +1,56 @@
+// MagicInfoBox.tsx
+import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+
+type MagicInfoBoxProps = {
+  title: string
+  color: string
+  subtitle: JSX.Element
+  subtitle2: JSX.Element
+  content: JSX.Element
+  description: JSX.Element
+}
+
+const MagicInfoBox = ({
+  title,
+  color,
+  subtitle,
+  subtitle2,
+  content,
+  description,
+}: MagicInfoBoxProps) => {
+  const bgClass = color === "green" ? "bg-ada-magicGreen" : "bg-ada-white2"
+
+  return (
+    <div className="text-center text-black max-w-[900px] mt-10 m-auto mx-4">
+      <div
+        className={`border-ada-orange border rounded-3xl px-12 py-4 mx-auto ${bgClass}`}
+      >
+        <p className="text-adaDesc">{subtitle}</p>
+        <p className="text-adaSubtitleThird md:text-[150px] font-anton text-ada-orange">
+          {title}
+        </p>
+        <p className="text-adaDesc">{subtitle2}</p>
+      </div>
+      <div>{content}</div>
+      <div
+        className={`border-ada-orange border rounded-3xl px-12 py-4 mx-auto text-adaDesc ${bgClass}`}
+      >
+        {description}
+      </div>
+      <div className="flex justify-center items-center mx-auto z-2 mt-10">
+        <a href="#konsultacja">
+          <StaticImage
+            src={"../../images/magicBtn.svg"}
+            alt=""
+            placeholder="blurred"
+            width={80}
+            height={80}
+          />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default MagicInfoBox
---
+++ src/components/MagicBioBanner3/circleImages.tsx
@@ -0,0 +1,70 @@
+// circleImages.ts
+import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+
+export const CircleImage = ({ circleKey }: { circleKey: string }) => {
+  switch (circleKey) {
+    case "ada":
+      return (
+        <StaticImage
+          src="../../images/m1.webp"
+          alt=""
+          layout="fixed"
+          width={170}
+          height={170}
+          placeholder="blurred"
+          quality={90}
+        />
+      )
+    case "justyna":
+      return (
+        <StaticImage
+          src="../../images/m2.webp"
+          alt=""
+          layout="fixed"
+          width={170}
+          height={170}
+          placeholder="blurred"
+          quality={90}
+        />
+      )
+    case "dorota":
+      return (
+        <StaticImage
+          src="../../images/m3.webp"
+          alt=""
+          layout="fixed"
+          width={170}
+          height={170}
+          placeholder="blurred"
+          quality={90}
+        />
+      )
+    case "nicola":
+      return (
+        <StaticImage
+          src="../../images/m4.webp"
+          alt=""
+          layout="fixed"
+          width={170}
+          height={170}
+          placeholder="blurred"
+          quality={90}
+        />
+      )
+    case "papajka":
+      return (
+        <StaticImage
+          src="../../images/m5.webp"
+          alt=""
+          layout="fixed"
+          width={170}
+          height={170}
+          placeholder="blurred"
+          quality={90}
+        />
+      )
+    default:
+      return null
+  }
+}
