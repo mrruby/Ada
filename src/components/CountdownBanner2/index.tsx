0abbe8affb037b4de738a08bbe4023ba3f737201
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2025-09-03 22:45:30 +0200

    update countdown

--- src/components/CountdownBanner2/index.tsx
+++ src/components/CountdownBanner2/index.tsx
@@ -1,20 +1,37 @@
 import Typography from "components/shared/Typography"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import CountdownTimer from "../../helpers/CountdownTimer"
 import MaxWithBgColorContainer from "../Layout/MaxWithBgColorContainer"
 
 const CountdownBanner2: React.FC = () => {
   const targetDate = new Date("2025-09-10T17:59:59")
 
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY >= 70)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
-    <MaxWithBgColorContainer bgColor="fixed bottom-0 left-0 w-full z-50 bg-ada-light-pink flex flex-col items-center p-4 text-center">
-      <Typography
-        variant="h2"
-        className="text-ada-pink7 text-3xl md:text-4xl font-bold mb-4"
-      >
-        Nie czekaj - liczba miejsc ograniczona!
-      </Typography>
-      <CountdownTimer targetDate={targetDate} text="" />
+    <MaxWithBgColorContainer
+      bgColor={`fixed ${
+        isScrolled ? "top-0" : "top-[70px]"
+      } left-0 z-50 bg-ada-magicPink3  p-4 text-center`}
+    >
+      <div className="flex flex-col md:flex-row md:gap-4 justify-center items-center">
+        <Typography
+          variant="h3"
+          className="text-ada-pink7 text-xl md:text-2xl font-bold mb-2"
+        >
+          Nie czekaj - liczba miejsc ograniczona!
+        </Typography>
+        <CountdownTimer targetDate={targetDate} color="bg-ada-magicPink3" />
+      </div>
     </MaxWithBgColorContainer>
   )
 }
