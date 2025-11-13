b325a96d22eba6caa907707a575d658526687aeb
agnieszkastanczuk <work.a.stanczuk@gmail.com>
2025-11-13 21:27:28 +0100

    fix animations

--- src/components/MagicComments/index.tsx
+++ src/components/MagicComments/index.tsx
@@ -1,4 +1,3 @@
-import Section from "components/shared/Section"
 import Typography from "components/shared/Typography"
 import { StaticImage } from "gatsby-plugin-image"
 import React, { useState } from "react"
@@ -13,62 +12,64 @@ const CommentBlock1 = () => {
 
   return (
     <div
-      className="group flex flex-col items-start w-full max-w-screen-lg gap-4 mb-12"
+      className="flex flex-col items-start w-screen justify-center gap-4 mb-12"
       onMouseEnter={() => setAnimated(true)}
     >
-      <div
-        className={`
-    w-full flex justify-start ${transitionClass}
+      <div className="mx-auto">
+        <div
+          className={`
+    flex justify-start ${transitionClass}
     ${animated ? "opacity-100 scale-100" : "lg:opacity-0 scale-75"}
   `}
-      >
-        <div className="flex items-center lg:gap-12">
-          <StaticImage
-            loading="eager"
-            src="../../images/comment1a.webp"
-            alt="Komentarz 1a"
-            placeholder="blurred"
-            width={580}
-            height={333}
-            formats={["auto", "webp", "avif"]}
-            quality={100}
-          />
-          <StaticImage
-            src="../../images/commentIcon1a.png"
-            alt="Ikona 1a"
-            placeholder="none"
-            width={50}
-            height={50}
-            className="flex-shrink-0 opacity-0 lg:opacity-100"
-          />
+        >
+          <div className="flex items-center lg:gap-12">
+            <StaticImage
+              loading="eager"
+              src="../../images/comment1a.webp"
+              alt="Komentarz 1a"
+              placeholder="blurred"
+              width={580}
+              height={333}
+              formats={["auto", "webp", "avif"]}
+              quality={100}
+            />
+            <StaticImage
+              src="../../images/commentIcon1a.png"
+              alt="Ikona 1a"
+              placeholder="none"
+              width={50}
+              height={50}
+              className="flex-shrink-0 opacity-0 lg:opacity-100"
+            />
+          </div>
         </div>
-      </div>
-      <div
-        className={`
-    w-full flex justify-end ${transitionClass} delay-200 lg:-mt-12
+        <div
+          className={`
+    flex justify-end ${transitionClass} delay-200 lg:-mt-12
     ${animated ? "opacity-100 scale-100" : "lg:opacity-0 scale-75"}
     origin-right
   `}
-      >
-        <div className="flex items-center gap-24">
-          <StaticImage
-            src="../../images/commentIcon1b.png"
-            alt="Ikona 1b"
-            placeholder="none"
-            width={50}
-            height={50}
-            className="flex-shrink-0 opacity-0 lg:opacity-100"
-          />
-          <StaticImage
-            loading="eager"
-            src="../../images/comment1ba.webp"
-            alt="Komentarz 1b"
-            placeholder="blurred"
-            width={600}
-            height={356}
-            formats={["auto", "webp", "avif"]}
-            quality={100}
-          />
+        >
+          <div className="flex items-center gap-24">
+            <StaticImage
+              src="../../images/commentIcon1b.png"
+              alt="Ikona 1b"
+              placeholder="none"
+              width={50}
+              height={50}
+              className="flex-shrink-0 opacity-0 lg:opacity-100"
+            />
+            <StaticImage
+              loading="eager"
+              src="../../images/comment1ba.webp"
+              alt="Komentarz 1b"
+              placeholder="blurred"
+              width={600}
+              height={356}
+              formats={["auto", "webp", "avif"]}
+              quality={100}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -80,63 +81,64 @@ const CommentBlock2 = () => {
 
   return (
     <div
-      className="group flex flex-col items-start w-full max-w-screen-lg gap-4 mb-12"
+      className="flex flex-col items-start w-screen justify-center gap-4 mb-12"
       onMouseEnter={() => setAnimated(true)}
     >
-      <div
-        className={`
-    w-full flex justify-start ${transitionClass}
+      <div className="mx-auto">
+        <div
+          className={`
+    flex justify-start ${transitionClass}
     ${animated ? "opacity-100 scale-100" : "lg:opacity-0 scale-75"}
   `}
-      >
-        <div className="flex items-center lg:gap-12">
-          <StaticImage
-            loading="eager"
-            src="../../images/comment2a.webp"
-            alt="Komentarz 2a"
-            placeholder="blurred"
-            width={565}
-            height={178}
-            formats={["auto", "webp", "avif"]}
-            quality={100}
-          />
-          <StaticImage
-            src="../../images/commentIcon2a.png"
-            alt="Ikona 2a"
-            placeholder="none"
-            width={50}
-            height={50}
-            className="flex-shrink-0 opacity-0 lg:opacity-100"
-          />
+        >
+          <div className="flex items-center lg:gap-12">
+            <StaticImage
+              loading="eager"
+              src="../../images/comment2a.webp"
+              alt="Komentarz 2a"
+              placeholder="blurred"
+              width={565}
+              height={178}
+              formats={["auto", "webp", "avif"]}
+              quality={100}
+            />
+            <StaticImage
+              src="../../images/commentIcon2a.png"
+              alt="Ikona 2a"
+              placeholder="none"
+              width={50}
+              height={50}
+              className="flex-shrink-0 opacity-0 lg:opacity-100"
+            />
+          </div>
         </div>
-      </div>
-
-      <div
-        className={`
-    w-full flex justify-end ${transitionClass} delay-200 lg:-mt-12
+        <div
+          className={`
+    flex justify-end ${transitionClass} delay-200 lg:-mt-12
     ${animated ? "opacity-100 scale-100" : "lg:opacity-0 scale-75"}
     origin-right
   `}
-      >
-        <div className="flex items-center gap-24">
-          <StaticImage
-            src="../../images/commentIcon2b.png"
-            alt="Ikona 2b"
-            placeholder="none"
-            width={50}
-            height={50}
-            className="flex-shrink-0 opacity-0 lg:opacity-100"
-          />
-          <StaticImage
-            loading="eager"
-            src="../../images/comment2b.webp"
-            alt="Komentarz 2b"
-            placeholder="blurred"
-            width={574}
-            height={195}
-            formats={["auto", "webp", "avif"]}
-            quality={100}
-          />
+        >
+          <div className="flex items-center gap-24">
+            <StaticImage
+              src="../../images/commentIcon2b.png"
+              alt="Ikona 2b"
+              placeholder="none"
+              width={50}
+              height={50}
+              className="flex-shrink-0 opacity-0 lg:opacity-100"
+            />
+            <StaticImage
+              loading="eager"
+              src="../../images/comment2b.webp"
+              alt="Komentarz 2b"
+              placeholder="blurred"
+              width={574}
+              height={195}
+              formats={["auto", "webp", "avif"]}
+              quality={100}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -148,63 +150,64 @@ const CommentBlock3 = () => {
 
   return (
     <div
-      className="group flex flex-col items-start w-full max-w-screen-lg gap-4 mb-12"
+      className="flex flex-col items-start w-screen justify-center gap-4 mb-12"
       onMouseEnter={() => setAnimated(true)}
     >
-      <div
-        className={`
-    w-full flex justify-start ${transitionClass}
+      <div className="mx-auto">
+        <div
+          className={`
+    flex justify-start ${transitionClass}
     ${animated ? "opacity-100 scale-100" : "lg:opacity-0 scale-75"}
   `}
-      >
-        <div className="flex items-center lg:gap-12">
-          <StaticImage
-            loading="eager"
-            src="../../images/comment3a.webp"
-            alt="Komentarz 3a"
-            placeholder="blurred"
-            width={620}
-            height={223}
-            formats={["auto", "webp", "avif"]}
-            quality={100}
-          />
-          <StaticImage
-            src="../../images/commentIcon3a.png"
-            alt="Ikona 3a"
-            placeholder="none"
-            width={50}
-            height={50}
-            className="flex-shrink-0 opacity-0 lg:opacity-100"
-          />
+        >
+          <div className="flex items-center lg:gap-12">
+            <StaticImage
+              loading="eager"
+              src="../../images/comment3a.webp"
+              alt="Komentarz 3a"
+              placeholder="blurred"
+              width={620}
+              height={223}
+              formats={["auto", "webp", "avif"]}
+              quality={100}
+            />
+            <StaticImage
+              src="../../images/commentIcon3a.png"
+              alt="Ikona 3a"
+              placeholder="none"
+              width={50}
+              height={50}
+              className="flex-shrink-0 opacity-0 lg:opacity-100"
+            />
+          </div>
         </div>
-      </div>
-
-      <div
-        className={`
-    w-full flex justify-end ${transitionClass} delay-200 lg:-mt-12
+        <div
+          className={`
+    flex justify-end ${transitionClass} delay-200 lg:-mt-12
     ${animated ? "opacity-100 scale-100" : "lg:opacity-0 scale-75"}
     origin-right
   `}
-      >
-        <div className="flex items-center gap-24">
-          <StaticImage
-            src="../../images/commentIcon3b.png"
-            alt="Ikona 3b"
-            placeholder="none"
-            width={50}
-            height={50}
-            className="flex-shrink-0 opacity-0 lg:opacity-100"
-          />
-          <StaticImage
-            loading="eager"
-            src="../../images/comment3b.webp"
-            alt="Komentarz 3b"
-            placeholder="blurred"
-            width={568}
-            height={328}
-            formats={["auto", "webp", "avif"]}
-            quality={100}
-          />
+        >
+          <div className="flex items-center gap-24">
+            <StaticImage
+              src="../../images/commentIcon3b.png"
+              alt="Ikona 3b"
+              placeholder="none"
+              width={50}
+              height={50}
+              className="flex-shrink-0 opacity-0 lg:opacity-100"
+            />
+            <StaticImage
+              loading="eager"
+              src="../../images/comment3b.webp"
+              alt="Komentarz 3b"
+              placeholder="blurred"
+              width={568}
+              height={328}
+              formats={["auto", "webp", "avif"]}
+              quality={100}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -213,7 +216,7 @@ const CommentBlock3 = () => {
 
 const MagicComments = ({ version }: { version: number }) => {
   return (
-    <Section>
+    <div className="my-20 w-screen">
       {version === 1 && (
         <div className="flex flex-col items-center text-ada-black">
           <Typography variant="h1" className="mb-8 text-center animate-bounce">
@@ -225,7 +228,7 @@ const MagicComments = ({ version }: { version: number }) => {
           <CommentBlock3 />
         </div>
       )}
-    </Section>
+    </div>
   )
 }
 
