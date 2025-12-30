9631b8b2d478c377ed0c8732eb3c20ec7503a5a4
Dawid Urbas <durbas92@gmail.com>
2025-12-30 11:03:07 +0100

    Remove deps

--- src/components/LoadingDots/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const LoadingDots: React.FC = () => (
-  <span className="flex items-center">
-    <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce2 mx-0.5"></span>
-    <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce2 mx-0.5 animation-delay-200"></span>
-    <span className="w-2 h-2 bg-gray-500 rounded-full animate-bounce2 mx-0.5 animation-delay-400"></span>
-  </span>
-)
